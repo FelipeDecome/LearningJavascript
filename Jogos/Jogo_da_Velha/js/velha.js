f6c03c1db58c0467350c237c2e9132393bc30c1c
FelipeDecome <felipe.decome@hotmail.com>
2020-02-19 20:47:30 -0300

    velha

--- Jogos/Jogo_da_Velha/js/velha.js
+++ Jogos/Jogo_da_Velha/js/velha.js
@@ -1,33 +1,50 @@
 const wrapper = document.querySelector("#velha");
-const Xicon = "<i class='fas fa-times'></i>";
-const Oicon = "<i class='fas fa-circle'></i>";
+const stats = document.querySelector("#stats");
+const iconX = "<i class='fas fa-times'></i>";
+const iconO = "<i class='fas fa-circle'></i>";
 
-let grid = [
-  [null, null, null], // y
-  [null, null, null], // y
-  [null, null, null] // y
-  // x  x  x
-];
+let gameState = {
+  grid: [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ],
+  playerTurn: 1,
+  togglePlayerTurn() {
+    if (this.playerTurn == 0) {
+      this.playerTurn = 1;
+    } else if (this.playerTurn == 1) {
+      this.playerTurn = 0;
+    }
+  },
+  markField(l, c) {
+    if (this.grid[l][c] === '') {
+      this.grid[l][c] = this.playerTurn;
 
-let gameState = [
-  grid => [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-  ]
-  // turn
-];
+      this.togglePlayerTurn();
 
-function render(grid) {
+      render(this);
+      this.verify();
+      console.log(gameState);
+
+    }
+  },
+  verify() {
+  }
+};
+
+function render(gameState) {
   wrapper.innerHTML = null;
-  for (let l = 0; l < 3; l++) {
+  stats.innerHTML = null;
+
+  for (let l = 0; l < gameState.grid.length; l++) {
     let liRow = document.createElement("li");
     liRow.setAttribute("id", `row_${l}`);
     liRow.classList.add("row");
 
     let ul = document.createElement("ul");
 
-    for (let c = 0; c < 3; c++) {
+    for (let c = 0; c < gameState.grid[l].length; c++) {
       let liItem = document.createElement("li");
       liItem.setAttribute("id", `item_${l}-${c}`);
       liItem.classList.add("item");
@@ -35,15 +52,15 @@ function render(grid) {
       liItem.addEventListener("click", event => {
         event.preventDefault(event);
 
-        /* ! WHYYYYYYYYYYY */
-        MarkField(l, c, 1);
+        gameState.markField(l, c);
+        console.log(gameState);
       });
-      if (grid[l][c] == 0) {
+      if (gameState.grid[l][c] === 0) {
         liItem.classList.add("iconO");
-        liItem.innerHTML = Oicon;
-      } else if (grid[l][c] == 1) {
+        liItem.innerHTML = iconO;
+      } else if (gameState.grid[l][c] === 1) {
         liItem.classList.add("iconX");
-        liItem.innerHTML = Xicon;
+        liItem.innerHTML = iconX;
       }
       ul.append(liItem);
 
@@ -51,9 +68,14 @@ function render(grid) {
     }
     wrapper.append(liRow);
   }
+  let spanStats = document.createElement('span');
+  spanStats.setAttribute('id', 'player' + gameState.playerTurn);
+  spanStats.innerText = 'Player ' + -(gameState.playerTurn - 2);
+
+  stats.append(spanStats);
 }
 
-render(grid);
+render(gameState);
 
 function MarkField(l, c, player) {
   grid[l][c] = player;
