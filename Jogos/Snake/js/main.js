c3a1bf0804a6774a53b4b416f755678873ccdd4d
FelipeDecome <felipe.decome@hotmail.com>
2020-02-25 20:21:41 -0300

    teste

--- Jogos/Snake/js/main.js
+++ Jogos/Snake/js/main.js
@@ -3,6 +3,13 @@ const canvasSize = [600, 600];
 const snakeColor = "#1DC6A7";
 const foodColor = "#FF3F3F";
 
+const container = document.querySelector("#canvas-container");
+container.width = canvasSize[0];
+container.height = canvasSize[1];
+
+let ctx = container.getContext("2d");
+ctx.fillStyle = "#92B901";
+ctx.fillRect(50, 50, 100, 100);
 
 
 
