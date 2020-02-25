9b5ee0d0a7bddc9865fc569ec0429a8ac5edd5d3
FelipeDecome <felipe.decome@hotmail.com>
2020-02-25 19:12:09 -0300

    Update main.js

--- Jogos/Snake/js/main.js
+++ Jogos/Snake/js/main.js
@@ -3,31 +3,7 @@ const canvasSize = [600, 600];
 const snakeColor = "#1DC6A7";
 const foodColor = "#FF3F3F";
 
-let Game = {
-  Canvas: {
-    width: canvasSize[0],
-    height: canvasSize[1]
-  },
-  Snake: {
-    body: [],
-    x: 0,
-    y: 0,
-    color: snakeColor,
-    get snake() {
-      return this;
-    }
-  },
-  Food: {
-    x: 0,
-    y: 0,
-    color: foodColor,
-    get position() {
-      return [this.x, this.y];
-    }
-  },
-  colide() {},
-  getContainer() {
-    return document.querySelector(containerId);
-  }
-};
+
+
+
 console.log(Game);
