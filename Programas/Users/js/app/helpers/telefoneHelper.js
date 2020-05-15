27579e10cf387df7e01179634101f4e9d71c35ce
FelipeDecome <felipe.decome@hotmail.com>
2020-05-14 21:29:29 -0300

    program

--- Programas/Users/js/app/helpers/telefoneHelper.js
+++ Programas/Users/js/app/helpers/telefoneHelper.js
@@ -1,20 +1,9 @@
 class TelefoneHelper {
-  constructor(num) {
-    if (this._valida(num)) throw new Error("O telefone informado não está no formato correto '${num}. O número deve estar no formato ' (99) 12345-6789.'");
-    this._num = num;
+  constructor() {
+    throw new Error("Essa classe não deve ser instanciada.");
   }
 
-  //dd ttttt tttt
-  _valida(num) {
-    return /^\d{2} \d{4,5} \d{4}$/.test(num);
-  }
-
-  //(dd) ttttt-tttt
-  //dd ttttt tttt
-  _limpaTel(num) {
-    return num
-      .split(" ")
-      .map((telefone, index) => (index % 2) ? telefone.split("-") : telefone.substr(1, 2))
-      .join(" ");
+  static valida(num) {
+    return /^\(\d{2}\)\d{4,5}-\d{4}$/.test(num);
   }
 }
